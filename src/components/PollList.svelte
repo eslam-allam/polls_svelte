db624d697f109e8e3cfbbfd2b3a5dda42e43f35b
Eslam Allam <eslamallam73@gmail.com>
2024-03-22 07:36:30 +0300

    feat: Add poll details card animations

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,11 +1,13 @@
 <script>
+  import { fade, scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
   import PollStore from "../stores/PollStore.js";
   import PollDetails from "./PollDetails.svelte";
 </script>
 
 <div class="poll-list">
-  {#each $PollStore as poll (poll.id) }
-    <div>
+  {#each $PollStore as poll (poll.id)}
+    <div in:fade|global out:scale animate:flip={{ duration: 500 }}>
       <PollDetails {poll} />
     </div>
   {/each}
